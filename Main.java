58997ce19209e0fc152b1a1648737ed50ef9c3d3
9831023 <9831023@ceit.local>
2020-04-14 22:05:22 +0430

    the Main Class is a prototype to use when compiling different modules of the programm

--- Main.java
+++ Main.java
@@ -1,4 +1,51 @@
 package ir.ac.aut;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+//        System.out.println("\033[33m"+"              Welcome to Uno"+"\n");
+//        System.out.println("Do you want to play against Humans or against AI?");
+//        System.out.println("\033[4;31m"+"1)"+"\u001b[33m"+" Humans\n"+"\033[4;31m"+"2)"+"\u001b[33m"+" AI");
+//        int input=(new Scanner(System.in)).nextInt();
+//        while(input!=1&&input!=2){
+//            System.out.println("Please enter a valid number");
+//            input=(new Scanner(System.in)).nextInt();
+//        }
+//        if(input==1){
+//            System.out.println("\033[4;34m"+"Select the number of Players");
+//            System.out.println("\033[4;31m"+"1)"+"\u001b[33m"+" 2 players\n"+"\033[4;31m"+"2)"+"\u001b[33m"+" 3 Players"+"\033[4;31m"+"3) "+"\u001b[33m"+"4 players");
+//            int numberOfPlayers=(new Scanner(System.in)).nextInt();
+//            while(numberOfPlayers<1||numberOfPlayers>3){
+//                System.out.println("Please enter a valid number");
+//                numberOfPlayers=(new Scanner(System.in)).nextInt();
+//            }
+//        }else{
+//            System.out.println("Still working on it :)");
+//        }
+
+
+        NumericCart myNumericCart1= new NumericCart(0, COLOR.BLUE);
+        NumericCart myNumericCart2= new NumericCart(1, COLOR.BLUE);
+        NumericCart myNumericCart3= new NumericCart(2, COLOR.BLUE);
+        NumericCart myNumericCart4= new NumericCart(3, COLOR.BLUE);
+        NumericCart myNumericCart5= new NumericCart(4, COLOR.BLUE);
+        NumericCart myNumericCart6= new NumericCart(5, COLOR.BLUE);
+        NumericCart myNumericCart7= new NumericCart(6, COLOR.BLUE);
+        NumericCart myNumericCart8= new NumericCart(7, COLOR.BLUE);
+        NumericCart myNumericCart9= new NumericCart(8, COLOR.BLUE);
+        NumericCart myNumericCart10= new NumericCart(9, COLOR.BLUE);
+
+        int numberOfCarts=7;
+        while(numberOfCarts>0) {
+            numberOfCarts-=7;
+            if()
+        }
+
+
+
+//        Game Uno = new Game()
+    }
 }
