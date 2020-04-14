31f943a7aecfe7b49eefea3a4a074da76bd3a45a
9831023 <9831023@ceit.local>
2020-04-15 01:47:39 +0430

    created new class for this kind of cart

--- Draw2Cart.java
+++ Draw2Cart.java
@@ -1,4 +1,16 @@
 package ir.ac.aut;
 
-public class Draw2Cart {
+public class Draw2Cart extends Cart {
+    public Draw2Cart(COLOR colorOfCart){
+        super(20, colorOfCart);
+    }
+
+    @Override
+    public boolean canPlayCart(Cart lastCartPlayed){
+        if(lastCartPlayed instanceof Draw2Cart || lastCartPlayed.getColor().equals(this.color)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
